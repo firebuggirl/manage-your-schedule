223474827154286514b5f2b41bbe64831c79ab66
Juliette <julietteredheadgirl@yahoo.com>
2019-06-07 13:08:37 -0500

    validate password in user model + UserSchema.virtual('todos')

--- models/user.js
+++ models/user.js
@@ -1,12 +1,17 @@
-var mongoose = require('mongoose');
-var bcrypt = require('bcryptjs'); //password hashing function
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs'); //password hashing function
 const validator = require('validator');//validate email
 const passportLocalMongoose = require('passport-local-mongoose');
 const mongoSanitize = require('express-mongo-sanitize');//added June 20th, test to see if working...
 mongoose.Promise = global.Promise;
 
 
-var UserSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
     email: {
       type: String,
       unique: true,
@@ -15,50 +20,26 @@ var UserSchema = new mongoose.Schema({
       validate: [validator.isEmail, 'Invalid Email Address'],
       trim: true
     },
-    name: {
-      type: String,
-      required: true,
-      trim: true
-    },
-  //   favoriteBook: {
-  //     type: String,
-  //     required: true,
-  //     trim: true
-  //   },
-  //   favoriteBand: {
-  //     type: String,
-  //     required: true,
-  //     trim: true
-  //   },
-  //   favoriteFood: {
-  //     type: String,
-  //     required: true,
-  //     trim: true
-  //   },
-  //   text: {
-  //     type: String
-  //   //  ref: 'User'
-  // },
-  // start:{
-  //   type: Date
-  // },
     password: {
       type: String,
-      required: true
+      //minlength: 7,
+      required: true,
+      validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain "password"')
+            }
+        }
+
     },
     resetPasswordToken: String,
-    resetPasswordExpires: Date,
-  //   data: {
-  //   oauth: { type: String, required: true }
-  // }
-    // newFavoriteBook: {
-    //      type: String,
-    //      required: true,
-    //      trim: true
-    //    }
+    resetPasswordExpires: Date
 });
 
-
+UserSchema.virtual('todos', {
+    ref: 'Todo',
+    localField: '_id',
+    foreignField: 'todoId'
+})
 
 mongoSanitize.sanitize(UserSchema); //added Tues, June 20th...check to see if working
 
@@ -73,7 +54,7 @@ UserSchema.statics.authenticate = function(email, password, callback) {
         if (error) {
           return callback(error);
         } else if ( !user ) {
-          var err = new Error('User not found.');
+          const err = new Error('User not found.');
           err.status = 401;
           return callback(err);
         }
@@ -88,7 +69,7 @@ UserSchema.statics.authenticate = function(email, password, callback) {
 };
 // hash password before saving to database
 UserSchema.pre('save', function(next) {
-  var user = this;
+  const user = this;
   bcrypt.hash(user.password, 10, function(err, hash) {
     if (err) {
       return next(err);
@@ -97,24 +78,7 @@ UserSchema.pre('save', function(next) {
     next();
   });
 });
-var User = mongoose.model('User', UserSchema);//model method creates schema
+const User = mongoose.model('User', UserSchema);//model method creates schema
 module.exports = User;
 
 UserSchema.plugin(passportLocalMongoose, { usernameField: 'email' });
-
-// var UserSchema2 = new mongoose.Schema({
-//
-//     newFavoriteBook: {
-//       type: String,
-//       required: true,
-//       trim: true
-//     }
-// });
-// //
-//  var UserUpdate = mongoose.model('UserUpdate', UserSchema2);
-//  module.exports = UserUpdate;
-
-
-//Note:
-//was able to update favoriteBook in shell:
-// db.users.update({_id:ObjectId("5898e0659946de61e3e3e698")},{$set:{favoriteBook:"Rolling Stones Biography"}})
